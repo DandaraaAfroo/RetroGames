f69be66afe9967b50ffc7ef1732c649d7d45be86
unknown <dandarad196@gmail.com>
2025-01-03 14:32:02 -0300

    Task-01: Criação da Model Categoria e Produto

---
+++ src/main/java/com/generation/retrogame/RetrogameApplication.java
@@ -0,0 +1,13 @@
+package com.generation.retrogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrogameApplication.class, args);
+	}
+
+}
